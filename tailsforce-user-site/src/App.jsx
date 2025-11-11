aa420cec96b5f03b79c0300b0620bd53b50c522b
made-by-max <madebymax.dev@gmail.com>
2025-11-10 19:28:23 -0800

    initial commit

---
+++ tailsforce-user-site/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import PetGrid from "./components/PetGrid";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <PetGrid />
+    </div>
+  );
+}
---
+++ tailsforce-user-site/src/components/PetCard.jsx
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+
+const PetCard = ({ name, breed, image_link, sex, age }) => {
+  return (
+    <Wrapper>
+      <ImageWrapper>
+        <Image alt={name} src={image_link} />
+      </ImageWrapper>
+
+      <ContentWrapper>
+        <Name>{name}</Name>
+        <Text>{breed}</Text>
+        <Text>
+          {age} &middot; {sex}
+        </Text>
+      </ContentWrapper>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.article`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ImageWrapper = styled.div``;
+
+const Image = styled.img`
+  width: 100%;
+  aspect-ratio: 5 / 4;
+  object-fit: cover;
+  border-radius: 8px;
+`;
+
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+`;
+
+const Name = styled.h2`
+  font-family: "Figtree";
+  font-weight: 700;
+  margin-block: 0;
+  src: url(https://design.penpot.app/internal/gfonts/font/figtree/v9/_Xmz-HUzqDCFdgfMsYiV_F7wfS-Bs_eYR25XyEAk4A.woff2)
+    format("woff2");
+  font-size: 1.75rem;
+  color: #222222;
+`;
+
+const Text = styled.p`
+  font-family: "Figtree";
+  font-weight: 400;
+  src: url(https://design.penpot.app/internal/gfonts/font/figtree/v9/_Xmz-HUzqDCFdgfMsYiV_F7wfS-Bs_eYR25XyEAk4A.woff2)
+    format("woff2");
+  font-size: 1rem;
+  color: #6a6a6a;
+  margin-block: 0;
+`;
+
+export default PetCard;
